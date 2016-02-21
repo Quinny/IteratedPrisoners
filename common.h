1aaebd0a847d099b2eb50de86ce626582ea828f3
Quinn Perfetto <thequinnftw@gmail.com>
2016-02-21 15:09:01 -0500

    broke play_tourny into two functions

--- common.h
+++ common.h
@@ -59,13 +59,19 @@ int play(prisoner_t p1, prisoner_t p2, int n) {
     return p1_score;
 }
 
+// This could be used as a fitness function
+int total_score(prisoner_t p, std::vector<prisoner_t> ps, int n) {
+    int score = 0;
+    for (auto i: ps)
+        score += play(p, i, n);
+    return score;
+}
+
 std::vector<std::pair<prisoner_t, int>>
 play_tourny(std::vector<prisoner_t> players, int n) {
     std::vector<std::pair<prisoner_t, int>> ret;
     for (auto p1: players) {
-        int score = 0;
-        for (auto p2: players)
-           score += play(p1, p2, n);
+        auto score = total_score(p1, players, n);
         ret.push_back({p1, score});
     }
     return ret;

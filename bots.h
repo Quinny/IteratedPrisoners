4408da187b394efc6d6118a81892abd3dd33d0f1
Quinn Perfetto <thequinnftw@gmail.com>
2016-02-21 16:53:36 -0500

    added bots

--- bots.h
+++ bots.h
@@ -1,6 +1,8 @@
 #ifndef BOTS_H
 #define BOTS_H
 
+#include <algorithm>
+
 #include "common.h"
 
 // TODO
@@ -23,10 +25,58 @@ decision tft(history_t h) {
     return h.front();
 }
 
+decision sus_tft(history_t h) {
+    if (h.empty()) return decision::defect;
+    return h.front();
+}
+
+decision tf2t(history_t h) {
+    if (h.size() < 2)
+        return decision::cooperate;
+    if (h.front() == decision::defect && *std::next(h.begin()) == decision::defect)
+        return decision::defect;
+    return decision::cooperate;
+}
+
+decision sus_tf2t(history_t h) {
+    if (h.size() < 2)
+        return decision::defect;
+    if (h.front() == decision::defect && *std::next(h.begin()) == decision::defect)
+        return decision::defect;
+    return decision::cooperate;
+}
+
+decision grudger(history_t h) {
+    if (std::find(h.begin(), h.end(), decision::defect) != h.end())
+        return decision::defect;
+    return decision::cooperate;
+}
+
+decision sucker(history_t h) {
+    if (std::find(h.begin(), h.end(), decision::cooperate) != h.end())
+        return decision::cooperate;
+    return decision::defect;
+}
+
+decision hesitant(history_t h) {
+    if (h.size() < 2)
+        return decision::defect;
+    if (h.front() == decision::cooperate &&
+            *std::next(h.begin()) == decision::cooperate)
+        return decision::cooperate;
+    return decision::defect;
+}
+
 const std::vector<prisoner_t> all = {
-    {"All cooperate", all_coop},
-    {"All defect",    all_defect},
-    {"Tit for tat",   tft}
+    {"All cooperate",     all_coop},
+    {"All defect",        all_defect},
+    {"Tit for tat",       tft},
+    {"Sus Tit for tat",   sus_tft},
+    {"Tit for 2 tat",     tf2t},
+    {"Grudger",           grudger},
+    {"Sucker",            sucker},
+    {"Sus Tit for 2 tat", sus_tf2t},
+    {"Hesitant",          hesitant}
 };
 
 }
--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 #include "common.h"
 #include "bots.h"
 
+template <typename T, typename U>
+std::ostream& operator << (std::ostream& out, std::pair<T, U> p) {
+    out << "(" << p.first << ", " << p.second << ")";
+    return out;
+}
+
 int main() {
     prisoner_t allc = {"all cooperate", bots::all_coop};
     prisoner_t alld = {"all defect", bots::all_defect};
